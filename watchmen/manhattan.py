a26a83f8d9029553f5050d696da11618005a75af
Daryan Hanshew <dhanshew@infoblox.com>
2018-08-10 08:43:39 -0700

    Temporary fix for email spam

--- watchmen/manhattan.py
+++ watchmen/manhattan.py
@@ -35,18 +35,34 @@ NOT_A_FEED_ERROR = "ERROR: Feed does not exist or added in the wrong module!"
 
 SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:cyberintel-feeds-prod"
 
-FEEDS_TO_CHECK_HOURLY = {
-    'bambenek_c2_ip': {'metric_name': 'IPV4_TIDE_SUCCESS', 'min': 50, 'max': 300},
-    'cox_feed': {'metric_name': 'IPV4_TIDE_SUCCESS', 'min': 15000, 'max': 30000},
-    'Xylitol_CyberCrime': {'metric_name': 'URI', 'min': 30, 'max': 50},
-    'ecrimeX': {'metric_name': 'URI_TIDE_SUCCESS', 'min': 10, 'max': 400},
-    'G01Pack_DGA': {'metric_name': 'FQDN_TIDE_SUCCESS', 'min': 15, 'max': 35},
-    'tracker_h3x_eu': {'metric_name': 'URI', 'min': 5, 'max': 50},
-    'VX_Vault': {'metric_name': 'URI', 'min': 1, 'max': 20},
-    'Zeus_Tracker': {'metric_name': 'URI_TIDE_SUCCESS', 'min': 35, 'max': 55}
-}
+# FEEDS_TO_CHECK_HOURLY = {
+#     'bambenek_c2_ip': {'metric_name': 'IPV4_TIDE_SUCCESS', 'min': 50, 'max': 300},
+#     'cox_feed': {'metric_name': 'IPV4_TIDE_SUCCESS', 'min': 15000, 'max': 30000},
+#     'Xylitol_CyberCrime': {'metric_name': 'URI', 'min': 30, 'max': 50},
+#     'ecrimeX': {'metric_name': 'URI_TIDE_SUCCESS', 'min': 10, 'max': 400},
+#     'G01Pack_DGA': {'metric_name': 'FQDN_TIDE_SUCCESS', 'min': 15, 'max': 35},
+#     'tracker_h3x_eu': {'metric_name': 'URI', 'min': 5, 'max': 50},
+#     'VX_Vault': {'metric_name': 'URI', 'min': 1, 'max': 20},
+#     'Zeus_Tracker': {'metric_name': 'URI_TIDE_SUCCESS', 'min': 35, 'max': 55}
+# }
 
 FEEDS_TO_CHECK_DAILY = {
+    'bambenek_c2_ip': {'metric_name': 'IPV4_TIDE_SUCCESS', 'min': 50, 'max': 300, 'hour_submitted': '09'},
+
+    'cox_feed': {'metric_name': 'IPV4_TIDE_SUCCESS', 'min': 15000, 'max': 35000, 'hour_submitted': '09'},
+
+    'Xylitol_CyberCrime': {'metric_name': 'URI', 'min': 30, 'max': 50, 'hour_submitted': '09'},
+
+    'ecrimeX': {'metric_name': 'URI_TIDE_SUCCESS', 'min': 10, 'max': 400, 'hour_submitted': '09'},
+
+    'G01Pack_DGA': {'metric_name': 'FQDN_TIDE_SUCCESS', 'min': 15, 'max': 35, 'hour_submitted': '09'},
+
+    'tracker_h3x_eu': {'metric_name': 'URI', 'min': 5, 'max': 50, 'hour_submitted': '09'},
+
+    'VX_Vault': {'metric_name': 'URI', 'min': 1, 'max': 20, 'hour_submitted': '09'},
+
+    'Zeus_Tracker': {'metric_name': 'URI_TIDE_SUCCESS', 'min': 35, 'max': 55, 'hour_submitted': '09'},
+
     'CryptoLocker_DGA': {
         'metric_name': 'FQDN_TIDE_SUCCESS', 'min': 2500, 'max': 4500, 'hour_submitted': '09'
     },
@@ -94,11 +110,11 @@ def main(event, context):
     submitted_out_of_range_feeds = []
     event_type = event.get('type')
     try:
-        if event_type == HOURLY:
-            downed_feeds, submitted_out_of_range_feeds = watcher.process_feeds_metrics(
-                FEEDS_TO_CHECK_HOURLY, TABLE_NAME, 0
-            )
-        elif event_type == DAILY:
+        # if event_type == HOURLY:
+        #     downed_feeds, submitted_out_of_range_feeds = watcher.process_feeds_metrics(
+        #         FEEDS_TO_CHECK_HOURLY, TABLE_NAME, 0
+        #     )
+        if event_type == DAILY:
             downed_feeds, submitted_out_of_range_feeds = watcher.process_feeds_metrics(
                 FEEDS_TO_CHECK_DAILY, TABLE_NAME, 1
             )
