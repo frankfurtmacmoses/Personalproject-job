c63de2a8b199cbf57db7660298b1c594ff7334fe
Kayla Ramos <ramosk@cideskkr.r11.com>
2019-01-23 11:56:24 -0800

    Added SNS calls and modified message constants

--- watchmen/spectre.py
+++ watchmen/spectre.py
@@ -6,7 +6,7 @@ This script monitors S3 ensuring data from the Georgia Tech Feed is properly tra
 @author: Kayla Ramos
 @email: kramos@infoblox.com
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 from logging import getLogger, basicConfig, INFO
 from utils.sns_alerts import raise_alarm
 from utils.universal_watchmen import Watchmen
@@ -15,17 +15,16 @@ import pytz
 LOGGER = getLogger("Spectre")
 basicConfig(level=INFO)
 
-#File Path Strings
 BUCKET_NAME = "cyber-intel"
-FILE_NAME = datetime.now().strftime('%Y') + "/" + datetime.now().strftime('%m') + "/gt_mpdns_" + \
-            datetime.now().strftime("%Y%m%d") + ".zip"
-FILE_PATH = "hancock/georgia_tech/" + FILE_NAME
+PATH_TO_FILES = "hancock/georgia_tech/"
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:HancockTest"
 
 #Message Strings
-SUCCESS_MESSAGE = "Georgia Tech Feed data found on S3! File found: " + FILE_NAME
-FAILURE_MESSAGE = "ERROR: " + FILE_NAME + " could not be found in hancock/georgia_tech! Please check S3 and Georgia Tech VM!"
-FILE_NOT_FOUND_ERROR = "File: " + FILE_NAME + \
-                       " not found on S3 in cyber-intel/hancock/georgia_tech! Georgia Tech data is missing, please view logs!"
+SUCCESS_MESSAGE = "Georgia Tech Feed data found on S3! File found: "
+FAILURE_MESSAGE = "could not be found in hancock/georgia_tech! Please check S3 and Georgia Tech VM!"
+FAIL_SUBJECT_MESSAGE = "Spectre georgia_tech data monitor detected a failure!"
+FILE_NOT_FOUND_ERROR = " not found on S3 in cyber-intel/hancock/georgia_tech! Georgia Tech data is missing, " \
+                       "please view logs!"
 EXCEPTION_MESSAGE = "Spectre monitor for Georgia Tech data failed due to an exception! Please check the logs!"
 
 
@@ -36,23 +35,37 @@ def main(event, context):
     :return: status of the file
     """
     watcher = Watchmen(bucket_name=BUCKET_NAME)
-    status = SUCCESS_MESSAGE
+    fn = get_s3_filename()
+    file_path = '{}{}'.format(PATH_TO_FILES, fn)
+    message = '{} {}'.format(SUCCESS_MESSAGE, fn)
     try:
-        found_file = watcher.validate_file_on_s3(FILE_PATH)
+        found_file = watcher.validate_file_on_s3(file_path)
         if not found_file:
-            LOGGER.info(FILE_NOT_FOUND_ERROR)
-            status = FAILURE_MESSAGE
-            #raise_alarm(SNS_TOPIC_ARN, FILE_NOT_FOUND_ERROR, FAIL_SUBJECT_MESSAGE)
+            LOGGER.info('File:  {} {}'.format(fn, FILE_NOT_FOUND_ERROR))
+            message = 'ERROR:  {} {}'.format(fn, FAILURE_MESSAGE)
+            raise_alarm(SNS_TOPIC_ARN, 'File:  {} {}'.format(fn, FILE_NOT_FOUND_ERROR), FAIL_SUBJECT_MESSAGE)
     except Exception as ex:
         LOGGER.error(ex)
         LOGGER.info(EXCEPTION_MESSAGE)
-        status = FAILURE_MESSAGE
-        #raise_alarm(SNS_TOPIC_ARN, EXCEPTION_MESSAGE, FAIL_SUBJECT_MESSAGE)
+        message = FAILURE_MESSAGE
+        raise_alarm(SNS_TOPIC_ARN, EXCEPTION_MESSAGE, FAIL_SUBJECT_MESSAGE)
 
-    print(status)
-    #
-    # LOGGER.info(status)
-    # return status
+
+    LOGGER.info(message)
+    #return status
+
+def get_s3_filename():
+    """
+    Gets yesterday's s3 filename
+    :return: filename
+    """
+    now = datetime.now()
+    yesterday = now - timedelta(days=1)
+    fn = yesterday.strftime('%Y') + "/" + yesterday.strftime('%m') + "/gt_mpdns_" + \
+         yesterday.strftime("%Y%m%d") + ".zip"
+    return fn
+
+
 
 if __name__ == '__main__':
     main(None, None)
