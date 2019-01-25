20c9ca5cfd662ab9a243f26cc208b5748ddba7f4
Kayla Ramos <ramosk@cideskkr.r11.com>
2019-01-25 08:37:43 -0800

    Reformatted Logger messages

--- tests/test_spectre.py
+++ tests/test_spectre.py
@@ -6,7 +6,7 @@ import pytz
 from mock import patch
 from moto import mock_s3
 
-from watchmen.spectre import SUCCESS_MESSAGE
+from watchmen.spectre import SUCCESS_MESSAGE, FAILURE_MESSAGE, EXCEPTION_MESSAGE
 
 
 class TestSpectre(unittest.TestCase):
@@ -14,6 +14,7 @@ class TestSpectre(unittest.TestCase):
     def setUp(self):
         self.example_yesterday = datetime(year=2018, month=12, day=18, tzinfo=pytz.utc)
         self.example_filename = "2018/12/gt_mpdns_20181217.zip"
+        self.example_exception_message = "Something is not working"
 
     @patch('watchmen.spectre.datetime')
     def test_get_s3_filename(self, mock_datetime):
@@ -26,12 +27,21 @@ class TestSpectre(unittest.TestCase):
     @patch('watchmen.spectre.raise_alarm')
     @patch('watchmen.spectre.Watchmen.validate_file_on_s3')
     @patch('watchmen.spectre.get_s3_filename')
-    @patch('watchmen.spectre.Watchmen')
-    def test_main(self,mock_watchman, mock_filename, mock_validate, mock_alarm):
+    def test_main(self, mock_filename, mock_validate, mock_alarm):
         mock_filename.return_value = self.example_filename
         # File exists
         mock_validate.return_value = True
-        expected_result = '{} {}'.format(SUCCESS_MESSAGE, self.example_filename)
+        expected_result = '{}{}'.format(SUCCESS_MESSAGE, self.example_filename)
+        returned_result = main(None, None)
+        self.assertEqual(expected_result, returned_result)
+        # File does not exist or data = 0
+        mock_validate.return_value = False
+        expected_result = 'ERROR: {}{}'.format(self.example_filename, FAILURE_MESSAGE)
+        returned_result = main(None, None)
+        self.assertEqual(expected_result, returned_result)
+        # An exception is thrown
+        mock_validate.side_effect = Exception(self.example_exception_message)
+        expected_result = 'ERROR: {}{}'.format(self.example_filename, FAILURE_MESSAGE)
         returned_result = main(None, None)
         self.assertEqual(expected_result, returned_result)
 
