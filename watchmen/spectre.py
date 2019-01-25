41c5ca2bdea347e94d3df0e935ec718135162611
Kayla Ramos <ramosk@cideskkr.r11.com>
2019-01-25 08:59:36 -0800

    Deleted main that was used for testing

--- watchmen/spectre.py
+++ watchmen/spectre.py
@@ -1,7 +1,8 @@
 """
 Created on January 22, 2019
 
-This script monitors S3 ensuring data from the Georgia Tech Feed is properly transferred.
+This script monitors S3 ensuring data from Georgia Tech Feed is properly transferred.
+If data is not found or has a value of 0, an alert is sent out.
 
 @author: Kayla Ramos
 @email: kramos@infoblox.com
@@ -15,16 +16,17 @@ import pytz
 LOGGER = getLogger("Spectre")
 basicConfig(level=INFO)
 
+# Filepath Strings
 BUCKET_NAME = "cyber-intel"
 PATH_TO_FILES = "hancock/georgia_tech/"
-SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:HancockTest"
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:Hancock"
 
-#Message Strings
+# Message Strings
 SUCCESS_MESSAGE = "Georgia Tech Feed data found on S3! File found: "
-FAILURE_MESSAGE = "could not be found in hancock/georgia_tech! Please check S3 and Georgia Tech VM!"
-FAIL_SUBJECT_MESSAGE = "Spectre georgia_tech data monitor detected a failure!"
+FAILURE_MESSAGE = "could not be found in hancock/georgia_tech! Please check S3 and Georgia Tech logs!"
+FAIL_SUBJECT_MESSAGE = "Spectre Georgia Tech data monitor detected a failure!"
 FILE_NOT_FOUND_ERROR = " not found on S3 in cyber-intel/hancock/georgia_tech! Georgia Tech data is missing, " \
-                       "please view logs!"
+                       "please view the logs!"
 EXCEPTION_MESSAGE = "Spectre monitor for Georgia Tech data failed due to an exception! Please check the logs!"
 
 
@@ -63,7 +65,3 @@ def get_s3_filename():
     yesterday = now - timedelta(days=1)
     fn = yesterday.strftime('%Y') + "/" + yesterday.strftime('%m') + "/gt_mpdns_" + yesterday.strftime("%Y%m%d") + ".zip"
     return fn
-
-
-if __name__ == '__main__':
-    main(None, None)
