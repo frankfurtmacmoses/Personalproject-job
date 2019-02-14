72ab7f1023ee358b97fdbac912ea59a0048de5ae
Kayla Ramos <ramosk@cideskkr.r11.com>
2019-02-14 14:27:17 -0800

    Added Logging and Error Handling

--- watchmen/jupiter.py
+++ watchmen/jupiter.py
@@ -9,9 +9,8 @@ If data is not found in the database or the endpoint does work correctly, an SNS
 """
 from logging import getLogger, basicConfig, INFO
 from utils.sns_alerts import raise_alarm
-from utils.universal_watchmen import Watchmen
 from common.svc_checker import ServiceChecker
-import requests, json
+import json
 
 LOGGER = getLogger("Jupiter")
 basicConfig(level=INFO)
@@ -21,11 +20,12 @@ CHECK_LOGS = "Please check logs for more details!"
 ERROR_MESSAGE = "There is an error with the Sockeye endpoints!"
 FOUND_FAILURES = "Endpoints Failed!"
 NO_RESULTS = "There are no results! Endpoint file might be empty. Please check logs and "
-NO_RESULTS_MESSAGE = "No endpoints were checked! The results from the check are empty! Please check the logs and "
-OPEN_FILE_ERROR = " could not be opened and caused an exception. Please check the logs and "
+NO_RESULTS_MESSAGE = "No endpoints were checked! The results from the service check is empty! Please check the logs and"
+OPEN_FILE_ERROR = " could not be opened and caused an exception. There might be a typo or format error in the file or" \
+                  " it may be empty. Please check the logs and "
 
 ENDPOINT_FILE = "endpoints.json"
-SNS_TOPIC_ARN = "I do not know what this should be right now"
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:Sockeye0"
 
 # pylint: disable=unused-argument
 def main(event, context):
@@ -33,6 +33,7 @@ def main(event, context):
     main function
     :return: status of Sockeye endpoints
     """
+    message = "Sockeye endpoints are looking good!"
     # Checking if endpoint file can be opened
     try:
         with open(ENDPOINT_FILE) as data_file:
@@ -42,6 +43,7 @@ def main(event, context):
         message = "ERROR: '{}'{}'{}'".format(ENDPOINT_FILE, OPEN_FILE_ERROR, ENDPOINT_FILE)
         LOGGER.info(message)
         raise_alarm(SNS_TOPIC_ARN, message, ERROR_MESSAGE + "\n\n" + str(ex))
+        return message
 
     ep = ServiceChecker(points)
     results = ep.start()
@@ -51,17 +53,19 @@ def main(event, context):
     if results['failure']:
         message = ""
         for failed in results['failure']:
-            fail = "'{}' failed because of error '{}'.".format(failed['name'], failed['_err'])
+            fail = "'{}' failed because of error '{}'. Check for empty file or dict.".format(failed['name'], failed['_err'])
             LOGGER.error(fail)
             message += fail + '\n'
-        print(CHECK_LOGS)
+        message += CHECK_LOGS
         raise_alarm(SNS_TOPIC_ARN, message, ERROR_MESSAGE)
+        return message
 
     # Checking if results is empty
-    if not results:
-        LOGGER.error("{}{}".format(NO_RESULTS, ENDPOINT_FILE))
-        message = "{}{}".format(NO_RESULTS_MESSAGE, ENDPOINT_FILE)
+    if not results['failure'] and not results['success']:
+        LOGGER.error("{} {}".format(NO_RESULTS, ENDPOINT_FILE))
+        message = "{} {}".format(NO_RESULTS_MESSAGE, ENDPOINT_FILE)
         raise_alarm(SNS_TOPIC_ARN, message, ERROR_MESSAGE)
+        return message
 
     return message
 
