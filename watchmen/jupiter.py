5449bd1014ffde65a5115bc50c25e6c892491ec2
Kayla Ramos <ramosk@cideskkr.r11.com>
2019-02-27 11:57:13 -0800

    Cleaned up main

--- watchmen/jupiter.py
+++ watchmen/jupiter.py
@@ -7,6 +7,7 @@ If data is not found in the database or the endpoint does work correctly, an SNS
 @author: Kayla Ramos
 @email: kramos@infoblox.com
 """
+import sys
 from logging import getLogger, basicConfig, INFO
 from utils.sns_alerts import raise_alarm
 
@@ -20,21 +21,25 @@ basicConfig(level=INFO)
 # Messages
 CHECK_LOGS = "Please check logs for more details!"
 ERROR_MESSAGE = "There is an error with the Sockeye endpoints!"
+EXIT_MESSAGE = "Cannot continue with check"
 FOUND_FAILURES = "Endpoints Failed!"
-NO_RESULTS = "There are no results! Endpoint file might be empty. Please check logs and "
-NO_RESULTS_MESSAGE = "No endpoints were checked! The results from the service check is empty! Please check the logs and"
+LOCAL_ENDPOINT_FILE = "endpoints.json"
+NO_RESULTS = "There are no results! Endpoint file might be empty. Please check logs and endpoint file " \
+             "(currently using local file)."
+NO_RESULTS_MESSAGE = "No endpoints were checked! The results from the service check is empty! Please check the logs " \
+                     "and endpoint file (currently using local file)"
 OPEN_FILE_ERROR = " could not be opened and caused an exception. There might be a typo or format error in the file or" \
                   " it may be empty. Please check the logs and "
 SUCCESS_MESSAGE = "Sockeye endpoints are looking good!"
 
-SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:Sockeye"
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:SockeyeTest"
 
 
-def notify(results, endpoint_file):
+def notify(results):
     """
     Send notifications to Sockeye topic if failed endpoints exist or no results exist at all
     :param results: dict to be checked for failed endpoints
-    :param endpoint_file: file name for notifications to know possible orirgn of some errors
+    :param endpoint_file: file name for notifications to know possible origin of some errors
     :return: the notification message
     """
     # Checking failure list and announcing errors
@@ -51,30 +56,32 @@ def notify(results, endpoint_file):
 
     # Checking if results is empty
     if not results['failure'] and not results['success']:
-        LOGGER.error("{} {}".format(NO_RESULTS, endpoint_file))
-        message = "{} {}".format(NO_RESULTS_MESSAGE, endpoint_file)
+        LOGGER.error("{}".format(NO_RESULTS))
+        message = "{}".format(NO_RESULTS_MESSAGE)
         raise_alarm(SNS_TOPIC_ARN, message, ERROR_MESSAGE)
         return message
 
+    # All Successes
+    return SUCCESS_MESSAGE
 
-def load_endpoints(endpoint_file):
+
+def load_endpoints():
     """
     Loads json file of endpoints.
     If an exception is thrown (meaning an error with opening and/or loading),
     an sns will be sent to the Sockeye Topic
-    :param endpoint_file: the file to be opened
-    :return: the endpoints or an exception
+    :return: the endpoints or exits upon exception
     """
     try:
-        with open(endpoint_file) as data_file:
+        with open(LOCAL_ENDPOINT_FILE) as data_file:
             points = json.load(data_file)
         return points
     except Exception as ex:
         LOGGER.error(ex)
-        message = "ERROR: '{}'{}'{}'".format(endpoint_file, OPEN_FILE_ERROR, endpoint_file)
+        message = "ERROR: '{}'{}'{}'".format(LOCAL_ENDPOINT_FILE, OPEN_FILE_ERROR, LOCAL_ENDPOINT_FILE)
         LOGGER.info(message)
         raise_alarm(SNS_TOPIC_ARN, message, ERROR_MESSAGE + "\n\n" + str(ex))
-        return message
+        sys.exit(EXIT_MESSAGE)
 
 
 # pylint: disable=unused-argument
@@ -83,20 +90,9 @@ def main(event, context):
     main function
     :return: status of Sockeye endpoints
     """
-    local_endpoint_file = "endpoints.json"
-    message = SUCCESS_MESSAGE
-    # If a list is not returned, return the message string
-    endpoints = load_endpoints(local_endpoint_file)
-    if not isinstance(endpoints, types.ListType):
-        return endpoints
-
-    # Check Endpoints
-    checked_points = ServiceChecker(endpoints)
-    results = checked_points.start()
-
-    # Checking endpoint results
-    ep_message = notify(results, local_endpoint_file)
-    if ep_message:
-        return ep_message
+    endpoints = load_endpoints()
+    checker = ServiceChecker(endpoints)
+    results = checker.start()
+    message = notify(results)
 
     return message
