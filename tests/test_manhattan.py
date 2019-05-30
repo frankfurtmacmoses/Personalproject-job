0079e2f30f51757847707e6ea255947473afb246
Kayla Ramos <ramosk@cideskkr.r11.com>
2019-05-30 09:46:10 -0700

    Refactored Manhattan

--- tests/test_manhattan.py
+++ tests/test_manhattan.py
@@ -2,7 +2,17 @@ from mock import patch
 from moto import mock_sns
 import unittest
 
-from watchmen.process.manhattan import main, SUCCESS_MESSAGE, FAILURE_MESSAGE
+from watchmen.process.manhattan import \
+    ABNORMAL_SUBMISSIONS_MESSAGE, \
+    ERROR_FEEDS, \
+    EXCEPTION_MESSAGE, \
+    FAILURE_MESSAGE, \
+    FEED_URL, \
+    PAGER_MESSAGE, \
+    STUCK_TASKS_MESSAGE, \
+    SUBJECT_MESSAGE, \
+    SUCCESS_MESSAGE
+from watchmen.process.manhattan import find_bad_feeds, find_stuck_tasks, main, notify, summarize, Watchmen
 
 
 class TestManhattan(unittest.TestCase):
@@ -13,106 +23,187 @@ class TestManhattan(unittest.TestCase):
         self.example_event_weekly = {'type': 'Weekly'}
         self.example_feed_name = "example_feed"
         self.example_exception_msg = "Thingy stopped working"
+        self.example_stuck_tasks = ["task1", "task2"]
+        self.example_found_bad_feeds = (["down_feed"], ["out_of_range"])
+        self.example_summarized_result = {
+            "subject": "Subject Line: Fail",
+            "message": "Message Body: things failed\nthis also failed\nEverything keeps failing!",
+            "success": False,
+            "pager_message": PAGER_MESSAGE,
+        }
+        self.example_notification = "The job was done"
+        self.example_watcher_group = "group"
+        self.example_down_feeds = ["down1", "down2", "down3", "down4"]
+        self.example_out_of_range_feeds = ["oor", "more out of range", "extra out of range"]
+        self.example_traceback = "line 4: file\n  error msg\n  more errors  \nError!"
+
+    @mock_sns
+    @patch('watchmen.process.manhattan.raise_alarm')
+    @patch('watchmen.process.manhattan.Watchmen.process_feeds_metrics')
+    @patch('watchmen.process.manhattan.Watchmen.process_feeds_logs')
+    def test_find_bad_feeds(self, mock_process_logs, mock_process_metrics, mock_alarm):
+        watcher = Watchmen(self.example_watcher_group)
+
+        mock_process_logs.return_value = self.example_down_feeds
+        mock_process_metrics.return_value = self.example_out_of_range_feeds
+
+        event_types = [self.example_event_hourly.get("type"),
+                       self.example_event_daily.get("type"),
+                       self.example_event_weekly.get("type")]
+
+        for event in event_types:
+            expected = mock_process_logs(), mock_process_metrics()
+            returned = find_bad_feeds(event, watcher)
+            self.assertEqual(expected, returned)
+
+        # Exception finding down feeds
+        mock_process_logs.side_effect = Exception('failure')
+        expected = None
+        returned = find_bad_feeds("Daily", watcher)
+        self.assertEqual(expected, returned)
+
+        # Exception finding out of range feeds
+        mock_process_metrics.side_effect = Exception('failure')
+        expected = None
+        returned = find_bad_feeds("Daily", watcher)
+        self.assertEqual(expected, returned)
 
     @mock_sns
     @patch('watchmen.process.manhattan.raise_alarm')
     @patch('watchmen.process.manhattan.Watchmen.get_stuck_ecs_tasks')
-    @patch('watchmen.process.manhattan.Watchmen.process_feeds_metrics')
-    @patch('watchmen.process.manhattan.Watchmen.process_feeds_logs')
-    def test_main(self, mock_process_logs, mock_process_feed, mock_get_stuck_tasks, mock_alarm):
-        tests = [
-            {
-                'stuck_tasks': [],
-                'downed_feeds': [],
-                'abnormal_feeds': [],
-                'event': self.example_event_hourly,
-                'alarm_call_count': 0,
-                'expected': SUCCESS_MESSAGE
-            },
-            {
-                'stuck_tasks': [],
-                'downed_feeds': [],
-                'abnormal_feeds': [],
-                'event': self.example_event_daily,
-                'alarm_call_count': 0,
-                'expected': SUCCESS_MESSAGE
-            },
-            {
-                'stuck_tasks': [],
-                'downed_feeds': [],
-                'abnormal_feeds': [],
-                'event': self.example_event_weekly,
-                'alarm_call_count': 0,
-                'expected': SUCCESS_MESSAGE
-            },
-            {
-                'stuck_tasks': ['task_got_stuck'],
-                'downed_feeds': [],
-                'abnormal_feeds': [],
-                'event': self.example_event_hourly,
-                'alarm_call_count': 2,
-                'expected': FAILURE_MESSAGE
-            },
-            {
-                'stuck_tasks': [],
-                'downed_feeds': ['downed_feed'],
-                'abnormal_feeds': [],
-                'event': self.example_event_hourly,
-                'alarm_call_count': 1,
-                'expected': FAILURE_MESSAGE
-            },
-            {
-                'stuck_tasks': [],
-                'downed_feeds': [],
-                'abnormal_feeds': ['submitted_out_of_range_feed'],
-                'event': self.example_event_hourly,
-                'alarm_call_count': 1,
-                'expected': FAILURE_MESSAGE
-            },
-            {
-                'stuck_tasks': [],
-                'downed_feeds': ['downed_feed'],
-                'abnormal_feeds': ['submitted_out_of_range_feed'],
-                'event': self.example_event_hourly,
-                'alarm_call_count': 1,
-                'expected': FAILURE_MESSAGE
-            },
-            {
-                'stuck_tasks': ['stuck_feed'],
-                'downed_feeds': ['downed_feed'],
-                'abnormal_feeds': [],
-                'event': self.example_event_hourly,
-                'alarm_call_count': 3,
-                'expected': FAILURE_MESSAGE
-            }
-        ]
-        for test in tests:
-            mock_get_stuck_tasks.return_value = test.get('stuck_tasks')
-            mock_process_logs.return_value = test.get('downed_feeds')
-            mock_process_feed.return_value = test.get('abnormal_feeds')
-            result = main(test.get('event'), None)
-            self.assertEqual(result, test.get('expected'))
-            self.assertEqual(mock_alarm.call_count, test.get('alarm_call_count'))
-            mock_alarm.reset_mock()
+    def test_find_stuck_tasks(self, mock_get_stuck, mock_alarm):
+        watcher = Watchmen(self.example_watcher_group)
+
+        # Everything works well
+        mock_get_stuck.return_value = self.example_stuck_tasks
+        expected = mock_get_stuck()
+        returned = find_stuck_tasks(watcher)
+        self.assertEqual(expected, returned)
+
+        # Exception occurred
+        mock_get_stuck.side_effect = Exception('failure')
+        expected = None
+        returned = find_stuck_tasks(watcher)
+        self.assertEqual(expected, returned)
+
+    @patch('watchmen.process.manhattan.notify')
+    @patch('watchmen.process.manhattan.summarize')
+    @patch('watchmen.process.manhattan.find_bad_feeds')
+    @patch('watchmen.process.manhattan.find_stuck_tasks')
+    def test_main(self, mock_find_stuck, mock_find_bad, mock_summarize, mock_notify):
+        # Check for stuck tasks
+        mock_find_stuck.return_value = self.example_stuck_tasks
+        stuck = mock_find_stuck()
+        self.assertIsNotNone(stuck)
+        mock_find_stuck.assert_called_with()
+        # Check for down or out of range feeds
+        mock_find_bad.return_value = self.example_found_bad_feeds
+        bad = mock_find_bad()
+        self.assertIsNotNone(bad)
+        mock_find_bad.assert_called_with()
+        #  Summarize the results
+        mock_summarize.return_value = self.example_summarized_result
+        summarized_results = mock_summarize()
+        self.assertIsNotNone(summarized_results)
+        mock_summarize.assert_called_with()
+        # Send alarm notification and return status of the feeds
+        mock_notify.return_value = self.example_notification
+        notification = mock_notify()
+        self.assertIsNotNone(notification)
+        mock_notify.assert_called_with()
+        # Check if a notification is returned
+        expected = notification
+        returned = main(self.example_event_hourly, None)
+        self.assertEqual(expected, returned)
 
     @mock_sns
     @patch('watchmen.process.manhattan.raise_alarm')
-    @patch('watchmen.process.manhattan.Watchmen.get_stuck_ecs_tasks')
-    @patch('watchmen.process.manhattan.Watchmen.process_feeds_metrics')
-    @patch('watchmen.process.manhattan.Watchmen.process_feeds_logs')
-    def test_main_exception(self, mock_process_logs, mock_process_feed, mock_get_stuck_tasks, mock_alarm):
+    def test_notify(self, mock_alarm):
+        bad_result = self.example_summarized_result
+        good_result = {
+            "subject": "Subject Line: Pass",
+            "message": "Message Body: Good Stuff",
+            "success": True,
+            "pager_message": PAGER_MESSAGE,
+        }
+
+        # Exception occurred
+        expected = "Failure: an exception occurred during checking process.\n{}".format(EXCEPTION_MESSAGE)
+        returned = notify(None)
+        self.assertEqual(expected, returned)
+
+        # Failure
+        expected = FAILURE_MESSAGE
+        returned = notify(bad_result)
+        self.assertEqual(expected, returned)
+
+        # Success
+        expected = SUCCESS_MESSAGE
+        returned = notify(good_result)
+        self.assertEqual(expected, returned)
+
+    def test_summarize(self):
         tests = [
-            {
-                'stuck_tasks': [],
-                'downed_feeds': [],
-                'abnormal_feeds': [],
-                'event': self.example_event_hourly,
-                'alarm_call_count': 1,
-                'expected': FAILURE_MESSAGE
-            }
+            {"stuck": self.example_stuck_tasks, "down": [], "oor": []},
+            {"stuck": [], "down": self.example_down_feeds, "oor": []},
+            {"stuck": [], "down": [], "oor": self.example_out_of_range_feeds},
+            {"stuck": self.example_stuck_tasks, "down": self.example_down_feeds, "oor": []},
+            {"stuck": self.example_stuck_tasks, "down": [], "oor": self.example_out_of_range_feeds},
+            {"stuck": [], "down": self.example_down_feeds, "oor": self.example_out_of_range_feeds},
+            {"stuck": self.example_stuck_tasks, "down": self.example_down_feeds, "oor": self.example_out_of_range_feeds}
         ]
+
         for test in tests:
-            mock_process_logs.side_effect = Exception('failure')
-            result = main(test.get('event'), None)
-            self.assertEqual(result, test.get('expected'))
-            mock_alarm.assert_called()
+            stuck_tasks = test.get("stuck")
+            down = test.get("down")
+            out_of_range = test.get("oor")
+
+            all_stuck = ""
+            all_down = ""
+            all_range = ""
+            for stuck in stuck_tasks:
+                all_stuck += "{}\n".format(stuck)
+            for down_feeds in down:
+                all_down += "{}\n".format(down_feeds)
+            for oor in out_of_range:
+                all_range += "{}\n".format(oor)
+
+            subject_line = SUBJECT_MESSAGE
+            message_body = ""
+            pager_message = ""
+            success = True
+            # Check for stuck tasks
+            if stuck_tasks:
+                subject_line += ' | Feeds ECS Cluster Has Hung Tasks'
+                message_body += STUCK_TASKS_MESSAGE.format(all_stuck, FEED_URL)
+                pager_message = PAGER_MESSAGE
+                success = False
+
+            # Check if any feeds are down or out of range
+            if down or out_of_range:
+                subject_line += ' | One or more feeds are down!'
+                message_body += "\n\n\n{}\n".format('-' * 60) if message_body else ""
+                message_body += '{}: {}\n{}\n{}\n{}\n{}\n'.format(
+                    self.example_event_daily.get('type'),
+                    FAILURE_MESSAGE,
+                    ERROR_FEEDS,
+                    all_down,
+                    ABNORMAL_SUBMISSIONS_MESSAGE,
+                    all_range)
+                success = False
+
+            expected = {
+                "subject": subject_line,
+                "message": message_body,
+                "success": success,
+                "pager_message": pager_message,
+            }
+            returned = summarize(self.example_event_daily.get("type"), stuck_tasks, (down, out_of_range))
+            self.assertEqual(expected, returned)
+
+        # If an exception occurred at some point in the process
+        stuck = None
+        bad = None
+        expected = None
+        returned = summarize(self.example_event_daily.get("type"), stuck, bad)
+        self.assertEqual(expected, returned)
--- watchmen/process/manhattan.py
+++ watchmen/process/manhattan.py
@@ -8,35 +8,40 @@ This script is designed to monitor daily, hourly, and weekly feeds and ensure pr
 
 """
 # Python imports
+import traceback
 from datetime import datetime, timedelta
 import pytz
 # Watchmen imports
-from logging import getLogger, basicConfig, INFO
 from watchmen.utils.universal_watchmen import Watchmen
 from watchmen.utils.sns_alerts import raise_alarm
+from watchmen.utils.logger import get_logger
 from watchmen.config import settings
-LOGGER = getLogger("Manhattan")
-basicConfig(level=INFO)
+
+LOGGER = get_logger("Manhattan", settings('logging.level', 'INFO'))
 
 DAILY = "Daily"
 HOURLY = "Hourly"
 WEEKLY = "Weekly"
 
-SUCCESS_MESSAGE = "feeds are up and running normally!"
-FAILURE_MESSAGE = "One or more feeds are down or submitting abnormal amounts of domains!"
-SUBJECT_MESSAGE = "Manhattan detected an issue with one or more feeds being down!"
-
-SUBJECT_EXCEPTION_MESSAGE = "Manhattan watchmen failed due to an exception!"
+ABNORMAL_SUBMISSIONS_MESSAGE = "Abnormal submission amount from these feeds: "
+EXCEPTION_BODY_MESSAGE = "Manhattan failed due to the following:\n\n{}\n\nPlease check the logs!"
 EXCEPTION_MESSAGE = "Please check logs for more details about exception!"
+FAILURE_MESSAGE = "One or more feeds are down or submitting abnormal amounts of domains!"
+PAGER_MESSAGE = 'One or more feeds have been running longer than a day! Please check your email for details!'
+STUCK_TASKS_MESSAGE = 'One or more feeds have been running longer than a day:\n{}\nThese feeds must be manually ' \
+                      'stopped within AWS console here: {}\n'
+SUBJECT_EXCEPTION_MESSAGE = "Manhattan watchmen failed due to an exception!"
+SUBJECT_MESSAGE = "Manhattan detected an issue"
+SUCCESS_MESSAGE = "feeds are up and running normally!"
 
 ERROR_FEEDS = "Downed feeds: "
-ABNORMAL_SUBMISSIONS_MESSAGE = "Abnormal submission amount from these feeds: "
-
-TABLE_NAME = "CyberInt-Reaper-prod-DynamoDbStack-3XBEIHSJPHBT-ReaperMetricsTable-1LHW3I46AEDQJ"
-NOT_A_FEED_ERROR = "ERROR: Feed does not exist or added in the wrong module!"
-
-SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:405093580753:cyberintel-feeds-prod"
 LOG_GROUP_NAME = 'feed-eaters-prod'
+TABLE_NAME = "CyberInt-Reaper-prod-DynamoDbStack-3XBEIHSJPHBT-ReaperMetricsTable-1LHW3I46AEDQJ"
+
+CLUSTER_NAME = settings('ecs.feeds.cluster')
+FEED_URL = settings('ecs.feeds.url')
+PAGER_SNS = settings('sns.pager')
+SNS_TOPIC_ARN = settings('manhattan.sns_topic', "arn:aws:sns:us-east-1:405093580753:Watchmen_Test")
 
 FEEDS_TO_CHECK_HOURLY = {
     'bambenek_c2_ip': {'metric_name': 'IPV4_TIDE_SUCCESS', 'min': 1, 'max': 20000},
@@ -105,31 +110,17 @@ FEEDS_WEEKLY_NAMES = [
 ]
 
 
-# pylint: disable=unused-argument
-def main(event, context):
+def find_bad_feeds(event_type, watcher):
     """
-    main function
-    :return: status of all hourly feeds
+    Find all the feeds that are down and/or out of range
+    @param event_type: Whether the check is Weekly, Daily, or Hourly
+    @param watcher: watchmen object
+    @return: tuple of lists: list of all the down feeds and list of out of range feeds or None upon exception
     """
-    watcher = Watchmen(log_group_name=LOG_GROUP_NAME)
-    status = SUCCESS_MESSAGE
     downed_feeds = []
     submitted_out_of_range_feeds = []
-    event_type = event.get('type')
+
     try:
-        cluster_name = settings('ecs.feeds.cluster')
-        stuck_tasks = watcher.get_stuck_ecs_tasks(cluster_name)
-        if stuck_tasks:
-            feed_url = settings('ecs.feeds.url')
-            pager_sns = settings('sns.pager')
-            subject = 'Feeds ECS Cluster has Hung Tasks!'
-            message = 'One or more feeds have been running longer than a day\n: ' + str(stuck_tasks) + \
-                      '\nThese feeds must be manually stopped within AWS console here: ' + feed_url
-            pager_message = 'One or more feeds have been running longer ' \
-                            'than a day! Please check your email for details!'
-            raise_alarm(pager_sns, pager_message, subject)
-            raise_alarm(SNS_TOPIC_ARN, message, subject)
-            status = FAILURE_MESSAGE
         end = datetime.now(tz=pytz.utc)
         if event_type == HOURLY:
             start = end - timedelta(hours=1)
@@ -149,18 +140,127 @@ def main(event, context):
             submitted_out_of_range_feeds = watcher.process_feeds_metrics(
                 FEEDS_TO_CHECK_WEEKLY, TABLE_NAME, 2
             )
+        return downed_feeds, submitted_out_of_range_feeds
     except Exception as ex:
-        raise_alarm(SNS_TOPIC_ARN, EXCEPTION_MESSAGE, SUBJECT_EXCEPTION_MESSAGE)
-        status = FAILURE_MESSAGE
         LOGGER.error(ex)
+        trace = traceback.format_exc(ex)
+        raise_alarm(SNS_TOPIC_ARN, EXCEPTION_BODY_MESSAGE.format(trace), SUBJECT_EXCEPTION_MESSAGE)
+        return None
 
-    if downed_feeds or submitted_out_of_range_feeds:
-        status = FAILURE_MESSAGE
-        message = (
-                event_type + ": " + status + '\n' + ERROR_FEEDS + str(downed_feeds) + '\n' +
-                ABNORMAL_SUBMISSIONS_MESSAGE + str(submitted_out_of_range_feeds)
-        )
-        LOGGER.info(message)
-        raise_alarm(SNS_TOPIC_ARN, message, SUBJECT_MESSAGE)
-    LOGGER.info(event_type + ": " + status)
+
+def find_stuck_tasks(watcher):
+    """
+    Find the task that are stuck in the given cluster
+    @param watcher: watchmen object
+    @return: list of all the stuck tasks or None upon exception
+    """
+    try:
+        stuck_tasks = watcher.get_stuck_ecs_tasks(CLUSTER_NAME)
+        return stuck_tasks
+    except Exception as ex:
+        LOGGER.error(ex)
+        trace = traceback.format_exc(ex)
+        raise_alarm(SNS_TOPIC_ARN, EXCEPTION_BODY_MESSAGE.format(trace), SUBJECT_EXCEPTION_MESSAGE)
+        return None
+
+
+# pylint: disable=unused-argument
+def main(event, context):
+    """
+    main function
+    :return: status of all hourly feeds
+    """
+    watcher = Watchmen(log_group_name=LOG_GROUP_NAME)
+    event_type = event.get('type')
+
+    stuck_tasks = find_stuck_tasks(watcher)
+    bad_feeds = find_bad_feeds(event_type, watcher)
+    summarized_result = summarize(event_type, stuck_tasks, bad_feeds)
+
+    LOGGER.info(summarized_result)
+    status = notify(summarized_result)
+
     return status
+
+
+def notify(summarized_result):
+    """
+    Using the results passed in, this function will send an email (and sometimes pager alarm) upon failures or exception
+    @param summarized_result: dict containing all information to send alert
+    @return: the status of the check
+    """
+    status = SUCCESS_MESSAGE
+
+    if summarized_result is None:
+        return "Failure: an exception occurred during checking process.\n{}".format(EXCEPTION_MESSAGE)
+
+    message = summarized_result.get("message")
+    subject = summarized_result.get("subject")
+    success = summarized_result.get("success")
+
+    if not success:
+        status = FAILURE_MESSAGE
+        raise_alarm(SNS_TOPIC_ARN, message, SUBJECT_MESSAGE)
+        if summarized_result.get("pager_message"):
+            raise_alarm(PAGER_SNS, PAGER_MESSAGE, subject)
+
+    return status
+
+
+def summarize(event_type, stuck_tasks, bad_feeds):
+    """
+    Summarizes the results from feed checks and creates a dict ready for email notifications
+    @param event_type: whether the check is Hourly, Daily, or Weekly
+    @param stuck_tasks: list of all the stuck tasks or None upon exception
+    @param bad_feeds: tuple of lists of the down feeds and the out of range feeds
+    @return: a dict to be readily used for the notification process
+    """
+    if stuck_tasks is None and bad_feeds is None:
+        return None
+
+    down = bad_feeds[0]
+    out_of_range = bad_feeds[1]
+
+    all_stuck = ""
+    all_down = ""
+    all_range = ""
+    for stuck in stuck_tasks:
+        all_stuck += "{}\n".format(stuck)
+    for down_feeds in down:
+        all_down += "{}\n".format(down_feeds)
+    for oor in out_of_range:
+        all_range += "{}\n".format(oor)
+
+    subject_line = SUBJECT_MESSAGE
+    message_body = ""
+    pager_message = ""
+    success = True
+    # Check for stuck tasks
+    if stuck_tasks:
+        subject_line += ' | Feeds ECS Cluster Has Hung Tasks'
+        message_body += STUCK_TASKS_MESSAGE.format(all_stuck, FEED_URL)
+        pager_message = PAGER_MESSAGE
+        success = False
+
+    # Check if any feeds are down or out of range
+    if down or out_of_range:
+        subject_line += ' | One or more feeds are down!'
+        message_body += "\n\n\n{}\n".format('-' * 60) if message_body else ""
+        message_body += '{}: {}\n{}\n{}\n{}\n{}\n'.format(
+            event_type,
+            FAILURE_MESSAGE,
+            ERROR_FEEDS,
+            all_down,
+            ABNORMAL_SUBMISSIONS_MESSAGE,
+            all_range)
+        success = False
+
+    # LOGGER.debug(subject_line)
+    # LOGGER.debug(message_body)
+
+    return {
+        "subject": subject_line,
+        "message": message_body,
+        "success": success,
+        "pager_message": pager_message
+    }
