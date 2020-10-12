4848f6be3bc983df394b4078bfe5e1c9d0fd691b
Phillip Hecksel <phecksel@infoblox.com>
2020-10-12 15:07:29 -0700

    Updated result_svc tests

--- tests/test_common_result_svc.py
+++ tests/test_common_result_svc.py
@@ -39,20 +39,46 @@ class TestResultSvc(unittest.TestCase):
         self.assertEqual(self.test_result_list, result_svc_obj.result_list)
         pass
 
-    @patch('watchmen.common.result_svc.json')
-    def test_load_notifiers(self, mock_json):
+    @patch('watchmen.common.result_svc.settings')
+    def test_build_test_sns_topic(self, mock_settings):
+
+        tests = [
+            {
+                "settings": 'arn:aws:sns:{region}:{account_id}:WatchmenTest',
+                "expected": 'arn:aws:sns:us-east-1:405093580753:WatchmenTest'
+            }, {
+                "settings": Exception(),
+                "expected": None
+            }
+        ]
+
+        for test in tests:
+            result_svc_obj = ResultSvc(self.test_result_list)
+            mock_settings.return_value = test.get("settings")
+            result = result_svc_obj._build_test_sns_topic()
+            self.assertEqual(result, test.get('expected'))
+
+    @patch('watchmen.common.result_svc.get_class')
+    def test_load_notifiers(self, mock_class):
         """
         test watchmen.common.result_svc :: ResultSvc :: _load_notifiers
         """
+        SNS = {
+            "Cyber-Intel Endpoints": {
+                "notifier": "SnsNotifier",
+                "sns": "arn:aws:sns:us-east-1:405093580753:Watchmen_Test"
+            }
+        }
+
         result_svc_obj = ResultSvc(self.test_result_list)
-        mock_json.load.return_value = self.test_notifier_dict
-        expected = self.test_notifier_dict
-        result = result_svc_obj._load_notifiers()
-        self.assertEqual(expected, result)
+        mock_class.return_value = SNS
+        expected = SNS
+        returned = result_svc_obj._load_notifiers()
+        self.assertEqual(expected, returned)
 
     def test_get_notifier(self):
         """
-        test watchmen.common.result_svc :: ResultSvc :: _ger_notifier
+        test watchmen.common.result_svc :: ResultSvc :: _get_notifier
         """
         result_svc_obj = ResultSvc(self.test_result_list)
 
@@ -85,6 +111,7 @@ class TestResultSvc(unittest.TestCase):
         test_result = TestResult()
         expected = self.test_sns_topic
         returned = result_svc_obj._get_sns_topic(test_result)
+        print(returned)
         self.assertEqual(expected, returned)
 
         # when target name not found
@@ -93,6 +120,14 @@ class TestResultSvc(unittest.TestCase):
 
         test_result = TestWrongResults
         expected = None
+        returned = result_svc_obj._get_sns_topic(test_result)
+        self.assertEqual(expected, returned)
+
+        class EnvironmentVarAlt:
+            target = 'Smartlisting'
+
+        test_result = EnvironmentVarAlt
+        expected = 'arn:aws:sns:us-east-1:405093580753:WatchmenTest'
         returned = result_svc_obj._get_sns_topic(test_result)
         self.assertEqual(expected, returned)
 
