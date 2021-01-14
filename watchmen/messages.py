9c2e7b57abd873898c9dd5f676f613647ad92ea6
Daryan Hanshew <dhanshew@infoblox.com>
2021-01-14 14:49:10 -0800

    Modified mothman messages to reference MTA

--- watchmen/messages.py
+++ watchmen/messages.py
@@ -165,21 +165,21 @@ MOLOCH = {
 }
 
 MOTHMAN = {
-    "exception_details": "There was an exception while trying to check the ForeverMail S3 files.\n\nTraceback of "
+    "exception_details": "There was an exception while trying to check the Malspam MTA S3 files.\n\nTraceback of "
                          "exception:\n{}",
-    "exception_subject": "EXCEPTION: Unable to check ForeverMail S3 files!",
+    "exception_subject": "EXCEPTION: Unable to check Malspam MTA S3 files!",
     "success_latest_hm": "Check was skipped due to latest S3 file being 0000.tar.gz.\nFile path: {}",
     "success_previous_file_dne": "Latest S3 file exists, but the previous S3 file does not. A notification"
                                  " should have already been sent for the previous file.\nPrevious file: {}",
     "success_previous_hm": "0010.tar.gz S3 file exists and the previous file is not expected to be uploaded.",
     "success_short_message": "SUCCESS: All expected S3 files exist.",
-    "success_subject": "SUCCESS: ForeverMail files found on S3",
+    "success_subject": "SUCCESS: Malspam MTA files found on S3",
     "success_unequal_files": "Two most recent files exist and are not the same file!\nLatest file: {}\n"
                              "Previous file: {}",
     "failure_latest_file_dne": "Latest file does not exist: {}",
     "failure_equal_files": "The same file was uploaded twice in a row.\nLatest file: {}\nPrevious file: {}",
     "failure_short_message": "FAILURE: S3 file(s) do not exist or files are the same size.",
-    "failure_subject": "FAILURE: ForeverMail S3 Files Error"
+    "failure_subject": "FAILURE: Malspam MTA S3 Files Error"
 }
 
 RORSCHACH = {
