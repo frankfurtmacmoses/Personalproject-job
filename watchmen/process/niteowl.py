65e077e7385eb4760e6f97c7bdc699738555b54a
Phillip Hecksel <phecksel@infoblox.com>
2021-03-26 14:44:05 -0700

    Added check for an event type
    This watchman will work similar to Rorschach and will rely on an event
    CloudWatch to pass an event type.

--- watchmen/process/niteowl.py
+++ watchmen/process/niteowl.py
@@ -14,14 +14,38 @@ to a repo. An exception represents an error that occurred in the code, or that o
 from watchmen import const, messages
 from watchmen.common.result import Result
 from watchmen.common.watchman import Watchman
+from watchmen.config import settings
+
+DAILY = "Daily"
 MESSAGES = messages.NITEOWL
+TARGET_ACCOUNT = settings("TARGET_ACCOUNT", "atg")
+
+EVENT_OFFSET_PAIR = {DAILY: 'days'}
+GENERIC_TARGET = 'Generic Github {}'.format(TARGET_ACCOUNT)
 
 
 class Niteowl(Watchman):
+
+    def __init__(self, event, context):
+        """
+        Niteowl Constructor
+        """
+        super().__init__()
+        self.event = event.get('Type')
 
     def monitor(self):
         """
         Monitors Github Targets in the github_targets.yaml file
         :return: <List> A list of result objects from the checks performed on each target.
         """
-        pass
+
+        if not self._is_valid_event():
+            pass
+
+    def _is_valid_event(self):
+        """
+        A check to make sure the event is supported by niteowl
+        :return: if the event is in the list of time offsets used for performing github checks
+        """
+        return self.event in list(EVENT_OFFSET_PAIR.keys())
+
