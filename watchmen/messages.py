71559b09ecae254a3e56ff7d6c7e7da6bca3907d
Phillip Hecksel <phecksel@infoblox.com>
2021-04-05 13:42:19 -0700

    Added a config target validator
    This is to ensure a target has all the required tags to make a github
    request. If it does not, a request will not be made and we dont waste
    out rate limit.

--- watchmen/messages.py
+++ watchmen/messages.py
@@ -165,6 +165,7 @@ NITEOWL = {
     "exception_invalid_event_details": "An invalid event was passed from the CloudWatch event. Check the events in the "
                                        "CloudFormation file.",
     "exception_invalid_event_subject": "EXCEPTION: Niteowl received an invalid event type!",
+    "exception_invalid_target_format": "Missing required tags: {}",
     "exception_message": "An exception occurred while checking Github targets. Check the logs for more details.",
 }
 
