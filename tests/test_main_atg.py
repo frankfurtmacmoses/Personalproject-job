e795553ae7ddc95abcce09d5700223bb342c093d
sfarheen1 <sfarheen@infoblox.com>
2021-06-07 17:28:33 +0530

    Added main_atg.py Unit Tests

--- tests/test_main_atg.py
+++ tests/test_main_atg.py
@@ -35,8 +35,9 @@ class MainAtgTester(unittest.TestCase):
 
     @patch('watchmen.process.bernard.Bernard')
     @patch('watchmen.process.bernard.Bernard.monitor')
+    @patch('watchmen.common.result_svc.ResultSvc.save_results')
     @patch('watchmen.common.result_svc.ResultSvc.send_alert')
-    def test_start_bernard_watcher(self, mock_alert, mock_monitor, mock_bernard):
+    def test_start_bernard_watcher(self, mock_alert, mock_save, mock_monitor, mock_bernard):
         mock_monitor.return_value = self.example_result_list
 
         expected = self.example_lambda_message + const.LINE_SEPARATOR
@@ -45,8 +46,9 @@ class MainAtgTester(unittest.TestCase):
 
     @patch('watchmen.process.comedian.Comedian')
     @patch('watchmen.process.comedian.Comedian.monitor')
+    @patch('watchmen.common.result_svc.ResultSvc.save_results')
     @patch('watchmen.common.result_svc.ResultSvc.send_alert')
-    def test_start_comedian_watcher(self, mock_alert, mock_monitor, mock_comedian):
+    def test_start_comedian_watcher(self, mock_alert, mock_save, mock_monitor, mock_comedian):
         mock_monitor.return_value = self.example_result_list
 
         expected = self.example_lambda_message + const.LINE_SEPARATOR
@@ -55,8 +57,9 @@ class MainAtgTester(unittest.TestCase):
 
     @patch('watchmen.process.jupiter.Jupiter')
     @patch('watchmen.process.jupiter.Jupiter.monitor')
+    @patch('watchmen.common.result_svc.ResultSvc.save_results')
     @patch('watchmen.common.result_svc.ResultSvc.send_alert')
-    def test_start_jupiter_watcher(self, mock_alert, mock_monitor, mock_jupiter):
+    def test_start_jupiter_watcher(self, mock_alert, mock_save, mock_monitor, mock_jupiter):
         mock_monitor.return_value = self.example_result_list
 
         expected = self.example_lambda_message + const.LINE_SEPARATOR
@@ -65,8 +68,9 @@ class MainAtgTester(unittest.TestCase):
 
     @patch('watchmen.process.manhattan.Manhattan')
     @patch('watchmen.process.manhattan.Manhattan.monitor')
+    @patch('watchmen.common.result_svc.ResultSvc.save_results')
     @patch('watchmen.common.result_svc.ResultSvc.send_alert')
-    def test_start_manhattan_watcher(self, mock_alert, mock_monitor, mock_manhattan):
+    def test_start_manhattan_watcher(self, mock_alert, mock_save, mock_monitor, mock_manhattan):
         mock_monitor.return_value = self.example_result_list
 
         expected = self.example_lambda_message + const.LINE_SEPARATOR
@@ -75,8 +79,9 @@ class MainAtgTester(unittest.TestCase):
 
     @patch('watchmen.process.metropolis.Metropolis')
     @patch('watchmen.process.metropolis.Metropolis.monitor')
+    @patch('watchmen.common.result_svc.ResultSvc.save_results')
     @patch('watchmen.common.result_svc.ResultSvc.send_alert')
-    def test_start_metropolis_watcher(self, mock_alert, mock_monitor, mock_metropolis):
+    def test_start_metropolis_watcher(self, mock_alert, mock_save, mock_monitor, mock_metropolis):
         mock_monitor.return_value = self.example_result_list
 
         expected = self.example_lambda_message + const.LINE_SEPARATOR
@@ -85,8 +90,9 @@ class MainAtgTester(unittest.TestCase):
 
     @patch('watchmen.process.mothman.Mothman')
     @patch('watchmen.process.mothman.Mothman.monitor')
+    @patch('watchmen.common.result_svc.ResultSvc.save_results')
     @patch('watchmen.common.result_svc.ResultSvc.send_alert')
-    def test_start_mothman_watcher(self, mock_alert, mock_monitor, mock_mothman):
+    def test_start_mothman_watcher(self, mock_alert, mock_save, mock_monitor, mock_mothman):
         mock_monitor.return_value = self.example_result_list
 
         expected = self.example_lambda_message + const.LINE_SEPARATOR
@@ -95,8 +101,9 @@ class MainAtgTester(unittest.TestCase):
 
     @patch('watchmen.process.silhouette.Silhouette')
     @patch('watchmen.process.silhouette.Silhouette.monitor')
+    @patch('watchmen.common.result_svc.ResultSvc.save_results')
     @patch('watchmen.common.result_svc.ResultSvc.send_alert')
-    def test_start_silhouette_watcher(self, mock_alert, mock_monitor, mock_silhouette):
+    def test_start_silhouette_watcher(self, mock_alert, mock_save, mock_monitor, mock_silhouette):
         mock_monitor.return_value = self.example_result_list
 
         expected = self.example_lambda_message + const.LINE_SEPARATOR
