f15b54990ab7cf3bf1b60d66756ea819542cb4f1
Frankfurt Ogunfunminiyi <oogunfunminiyi@infoblox.com>
2021-08-19 17:18:39 -0500

    Second commit, project well structured

--- watchmen/utils/dremio.py
+++ watchmen/utils/dremio.py
@@ -11,7 +11,12 @@ Note: A regular authenticated request (with a token):
 @email oogunfunminiyi@infoblox.com
 """
 
-
+import logging
+import boto3
+import boto3.session
+import json
+import base64
+from botocore.exceptions import ClientError
 import requests
 import traceback
 import logging
@@ -102,3 +107,40 @@ def pull_reflection_status(response):
     elements = json.loads(response)
     status = elements['data']['status']
     return status
+
+
+def get_secret(secret_name, region_name):
+    '''
+    Common library for utility functions
+
+    authors: lpeaslee, vcollooru
+    AWS
+    Given the secret name and the region of the Secret manager, this function will fetch the secret values
+    '''
+    secret = None
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        error_message = "Problems getting secret {secret_name} in {region_name} with exception {e}".format(
+            secret_name=secret_name,
+            region_name=region_name, e=e.response['Error']['Code'])
+        logging.error(error_message)
+        raise Exception(error_message)
+
+    # Decrypts secret using the associated KMS CMK.
+    # Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if 'SecretString' in get_secret_value_response:
+        secret = json.loads(get_secret_value_response['SecretString'])
+    else:
+        secret = json.loads(base64.b64decode(get_secret_value_response['SecretBinary']))
+    return secret
+
+
