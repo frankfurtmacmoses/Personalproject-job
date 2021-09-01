3be282a734098048db28e68a19a0306a86327758
Frankfurt Ogunfunminiyi <oogunfunminiyi@infoblox.com>
2021-09-01 09:16:56 -0500

    Commit on ozymandias

--- watchmen/process/ozymandias.py
+++ watchmen/process/ozymandias.py
@@ -23,14 +23,13 @@ from watchmen.config import settings
 
 CONFIG_NAME = settings('ozymandias.targets')
 
-SECRETE = settings('ozymandia.dremio_secret')
+SECRETE_PATH = settings('ozymandia.dremio_secret_path')
 ROOT_URL = settings('ozymandia.ROOT_URL')
 LOGIN_URL = settings('ozymandia.LOGIN_URL')
 REFLECTION_URL = settings('ozymandia.REFLECTION_URL')
 MESSAGES = messages.OZYMANDIAS
 REGION = settings("ozymandia.targets.region")
-user_name = settings("ozymandia.targets.region")
-final_result = {}
+secrete_name = settings("ozymandia.targets.secrete_name")
 CONFIG_PATH = os.path.join(
     os.path.realpath(os.path.dirname(__file__)), 'configs', CONFIG_NAME)
 
@@ -49,19 +48,7 @@ class Ozymandias(Watchman):
         Monitors Dremio Targets in the config.yaml file
         :return: <List> A list of result objects from the checks performed on each target.
         """
-
-        if not self._is_valid_event():
-            return self._create_invalid_event_result()
-
-        github_targets, tb = self._load_config()
-        if tb:
-            return self._create_config_not_loaded_result()
-
-        processed_targets = self._process_targets(github_targets)
-        summary_parameters = self._create_summary_parameters(processed_targets)
-        results = self._create_results(summary_parameters)
-        return results
-
+        self.create_details()
 
     @staticmethod
     def create_details():
@@ -69,16 +56,6 @@ class Ozymandias(Watchman):
         Creates details for the result objects from the new_change_strings and exception_strings if they exist.
         :param processed_target: <dict> A dictionary of the target name with any failures or exceptions that occurred
         :return: <str> A details string for the target with information from the checks performed
-        """
-        secret = dremio.get_secret(SECRETE, REGION)
-        """
-        split secrete dictionary to user_name and password, pass the outputs to dremio.generate_auth_token
-        """
-        token = dremio.generate_auth_token(str(list(secret.keys())), str(list(secret.values())))
-        reflection_list = dremio.get_reflection_list(token, REFLECTION_URL)
-
-        reflection_results = dremio.fetch_reflection_metadata(token, reflection_list, REFLECTION_URL)
-        """
         Return list containing dictionary of reflection result
          Get information about single reflection and return ->  "status": {
         "config": "OK",
@@ -88,21 +65,29 @@ class Ozymandias(Watchman):
         "failureCount": 0,
         "lastDataFetch": "2021-08-16T20:55:58.311Z",
         "expiresAt": "2021-08-16T23:55:58.311Z"
-                                 }   
+                                 }
         """
-
+        secret = dremio.get_secret(SECRETE_PATH, REGION)
+        token = secret[secrete_name]
+        user_name = secret['username']
+        auth_token = dremio.generate_auth_token(user_name, token, LOGIN_URL)
+        reflection_list = dremio.get_reflection_list(auth_token, REFLECTION_URL)
+        reflection_results = dremio.fetch_reflection_metadata(auth_token, REFLECTION_URL, reflection_list)
+        computed_result = {}
         for reflection_result in reflection_results:
             reflectionid, name, failurecount = reflection_result["id"], reflection_result["name"], \
                                                reflection_result["status"]['failureCount']
             if failurecount == 3:
-                final_result[reflectionid]: [name, failurecount]
-
+                computed_result[reflectionid]: {name: failurecount}
+        final_result = Ozymandias.determine_result(computed_result, datetime.now())
         return final_result
 
-    def determine_result(self, reflection_final_result,details=None):
+    def determine_result(self, reflection_final_result, details=None) -> {}:
         """
         Determine the outcome of the check
-
+        If {reflection_final_result} is not empty, Failed reflections present in {}
+        Otherwise, if no exception, No reflection failled, outcome is success
+        If connection to dremio or other errors? Exception is thrown.
         """
         if bool(reflection_final_result):
             disable_notifier = False
@@ -112,17 +97,13 @@ class Ozymandias(Watchman):
             success = False
 
         elif Exception:
-            ## Exception happened
+
             disable_notifier = False
             short_message = MESSAGES.get("reflection_exception_message")
             state = Watchman.STATE.get("exception")
             subject = MESSAGES.get("reflection_exception_subject")
             success = False
         else:
-            """
-            No reflection is found to have failed in the reflection 
-            final_result[]. Meaning all reflections passed
-            """
             disable_notifier = True,
             short_message = MESSAGES.get("reflection_success_message")
             state = Watchman.STATE.get("success")
@@ -140,13 +121,3 @@ class Ozymandias(Watchman):
             success=success,
             target=REFLECTION_URL
         )
-
-
-
-
-
-
-
-
-
-
